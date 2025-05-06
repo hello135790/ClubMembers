38162d077a57b49c4159691ee57cafef625bd748
Ashley <167535539+hello135790@users.noreply.github.com>
2025-05-06 03:17:57 +0000

    Starter code

---
+++ MemberInfo.java
@@ -0,0 +1,20 @@
+public class MemberInfo {
+    /**
+     * Constructs a MemberInfo object for the club member with name name,
+     * graduation year gradYear, and standing hasGoodStanding.
+     */
+    public MemberInfo(String name, int gradYear, boolean hasGoodStanding) {
+        /* implementation not shown */ }
+
+
+    /** Returns the graduation year of the club member. */
+    public int getGradYear() {
+        /* implementation not shown */ }
+
+
+    /** Returns true if the member is in good standing and false otherwise. */
+    public boolean inGoodStanding() {
+        /* implementation not shown */ }
+    // There may be instance variables, constructors, and methods that are not
+    // shown.
+}
